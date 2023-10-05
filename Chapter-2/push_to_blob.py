2d3edcf560f7304ff86f827c2b446420e6701a8c
Kedeisha <93458473+kedeisha1@users.noreply.github.com>
2023-10-04 23:41:32 -0400

    Update push_to_blob.py

--- Chapter-2/push_to_blob.py
+++ Chapter-2/push_to_blob.py
@@ -4,32 +4,28 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from io import BytesIO
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 functions = [league_table,top_scorers,detail_top,player_table,all_time_table,all_time_winner_club,top_scorers_seasons,goals_per_season]
 
 def to_blob(func):
-     """
-    Converts the output of a given function to Parquet format and uploads it to Azure Blob Storage.
 
+    '''
+    Converts the output of a given function to Parquet format and uploads it to Azure Blob Storage.
     Args:
         func (function): The function that retrieves data to be processed and uploaded.
-
     Returns:
         None
-
     This function takes a provided function, calls it to obtain data, and then converts the data into
     an Arrow Table. The Arrow Table is serialized into Parquet format and uploaded to an Azure Blob
     Storage container specified in the function. The function's name is used as the blob name.
-
     Example:
         Consider the function "top_scorers". Calling "to_blob(top_scorers)" will process the output
         of "top_scorers", convert it to Parquet format, and upload it to Azure Blob Storage.
-    """
-
+        '''
 
     file_name = func.__name__
     func = func()
@@ -41,7 +37,7 @@ def to_blob(func):
     parquet_buffer = BytesIO()
     pq.write_table(table, parquet_buffer)
 
-    connection_string = os.getenv('BLOB_CONN_STRING')
+    connection_string = 'Insert your blob storage connection key here'
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
     container_name = "testtech"
